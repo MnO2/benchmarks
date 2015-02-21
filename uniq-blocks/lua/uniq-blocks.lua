b9bb68fee0c9c15551ea429e40c0d435f8df7a90
Paul Meng <mno2@mno2.org>
2015-02-21 13:13:07 +0800

    Finish lua's implementation

--- uniq-blocks/lua/uniq-blocks.lua
+++ uniq-blocks/lua/uniq-blocks.lua
@@ -1,12 +1,30 @@
-function readAll(file)
-    local f = io.open(file, "rb")
-    local content = f:read("*all")
-    f:close()
-    return content
+function usage(program_name)
+    print("Usage: " .. program_name .. " filename")
 end
 
+function dedupeFile(file)
+    local f = io.open(file, "rb")
+    local t = {}
 
-local file = 'uniq-blocks.lua'
+    while true do
+        local block = f:read(512)
 
-c = readAll(file)
-print(c)
+        if not block then break end
+
+        if t[block] == nil then
+            t[block] = 1
+        else
+            t[block] = t[block] + 1
+        end
+    end
+
+    f:close()
+end
+
+if arg[1] == nil then
+    usage(arg[0])
+    do return end
+else
+    filename = arg[1]
+    dedupeFile(filename)
+end 
